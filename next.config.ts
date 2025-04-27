90c0b00c8f44db2fc5a95de63f1393c46eb4fbe5
tdyleuth <tony.dyleuth@gmail.com>
2025-04-26 23:08:01 -0700

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // VERY important for static export
+  images: {
+    unoptimized: true, // if you're using <Image>, this is needed for static export
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
